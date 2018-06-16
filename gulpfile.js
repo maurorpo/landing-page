c0633b5d3e743fb231b4e8e908a3c47417fc62e1
morerpo <morerpo@gmail.com>
2018-06-16 15:06:16 -0500

    Creación del proyecto, incluye: Gulp, Sass, Bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass'),
+    concat  = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify  = require('gulp-uglify'),
+    jshint  = require('gulp-jshint'),
+    rename  = require('gulp-rename'),
+    notify  = require('gulp-notify'),
+    minifyCSS   = require('gulp-minify-css'),
+    browserSync = require('browser-sync');
+
+var paths = {
+        in : {
+            public     : 'public/',
+            scripts    : 'resources/js/',
+            sass       : 'resources/sass/'
+        },
+        out : {
+            fonts      : 'public/fonts',
+            public     : 'public',
+            scripts    : 'public/js',
+            styles     : 'public/css'
+        }
+    };
+
+var files = {
+        css    : '*.css',
+        js     : '*.js',
+        html   : '*.html',
+        sass   : '*/*.scss',
+        script : 'script.js',
+        style  : 'style.scss'
+    };
+
+gulp.task('styles', function() {
+    return gulp.src( paths.in.sass + files.style )
+            .pipe( sass({ errLogToConsole: true }) )
+            .pipe( autoprefixer('last 5 version') )
+            .pipe( rename({ suffix: '.min' }) )
+            .pipe( minifyCSS() )
+            .pipe( gulp.dest(paths.out.styles) )
+            .pipe( notify( function(file) {
+                return 'SASS Compiler file: '+ file.relative;
+            }) );
+});
+
+gulp.task('scripts', function() {
+    return gulp.src( paths.in.scripts + files.script )
+        .pipe( jshint('.jshintrc') )
+        .pipe( jshint.reporter('default') )
+        .pipe( rename({ suffix: '.min' }) )
+        .pipe( uglify() )
+        .pipe( gulp.dest(paths.out.scripts) )
+        .pipe( notify( function(file) {
+            return 'Scripts Compiler file: '+ file.relative;
+        }) );
+});
+
+
+gulp.task('browser_sync', function() {
+    browserSync.init(null, {
+        server: { baseDir: paths.out.public }
+    });
+});
+
+gulp.task('bs_reload', function() {
+    browserSync.reload();
+});
+
+gulp.task('watch', function() {
+    gulp.watch( paths.in.sass + files.sass, ['styles'] );
+    gulp.watch( paths.in.scripts + files.js, ['scripts'] );
+    gulp.watch( paths.in.app + files.html, ['bs_reload'] );
+});
+
+gulp.task('default', [ 'styles', 'scripts', 'browser_sync', 'watch' ]);
---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+
+(function ($, window, document, undefined) {
+    'use strict';
+
+
+    function initReady() {
+
+    }
+    
+
+    $(document).ready(initReady);
+})(jQuery, window, document);
