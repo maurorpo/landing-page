2d720c79d01d6fcf24a1e74e8afb9ba71cf416e8
morerpo <morerpo@gmail.com>
2018-06-17 21:41:40 -0500

    Se creó la landing de prueba técnica para Chef, teniendo el cuenta el diseño envíado y las especificaciones

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,11 +2,21 @@
 (function ($, window, document, undefined) {
     'use strict';
 
+	function initReady() {
+	}
 
-    function initReady() {
-
-    }
+	function initLoad() {
+		$('.ico-open').on('click', function(){
+            $('#navbar').animate({"left": "0%"}, 'slow')
+        });
+        $('.ico-close').on('click', function(){
+            $('#navbar').animate({"left": "-100%"}, 'slow')
+        });
+        
+		skrollr.init();
+	}
     
 
-    $(document).ready(initReady);
+    $(document).on('ready', initReady);
+    $(window).on('load', initLoad);
 })(jQuery, window, document);
